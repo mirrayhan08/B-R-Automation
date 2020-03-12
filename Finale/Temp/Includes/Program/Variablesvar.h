5d5213af6c96c0a3b6b47bed9476dbbaf912d7bd
mirrayhan08 <mahedi.siu@gmail.com>
2020-03-12 01:10:18 +0100

    Project working

---
+++ Finale/Temp/Includes/Program/Variablesvar.h
@@ -0,0 +1,50 @@
+/* Automation Studio generated header file */
+/* Do not edit ! */
+
+#ifndef _BUR_1583943259_4_
+#define _BUR_1583943259_4_
+
+#include <bur/plctypes.h>
+
+/* Constants */
+#ifdef _REPLACE_CONST
+#else
+#endif
+
+
+/* Variables */
+_BUR_LOCAL unsigned char sourceX;
+_BUR_LOCAL unsigned char sourceY;
+_BUR_LOCAL unsigned char destinationY;
+_BUR_LOCAL unsigned char destinationX;
+_BUR_LOCAL plcbit test;
+_BUR_LOCAL plcbit waitingFlag;
+_BUR_LOCAL unsigned char flag;
+_BUR_LOCAL unsigned char prevFlag;
+_BUR_LOCAL plcbit iLinearMotionComplete;
+_BUR_LOCAL plcbit slider;
+_BUR_LOCAL plcbit iRotaryMotionComplete;
+_BUR_LOCAL plcbit isSliderStateBit;
+_BUR_LOCAL plcbit gripper;
+_BUR_LOCAL plcbit linearMotor;
+_BUR_LOCAL plcbit rotaryMotor;
+_BUR_LOCAL plcbit controlPos2;
+_BUR_LOCAL plcbit controlPos1;
+_BUR_LOCAL plcbit controlPos0;
+
+
+
+
+
+__asm__(".section \".plc\"");
+
+/* Used IEC files */
+__asm__(".ascii \"iecfile \\\"Logical/Program/Variables.var\\\" scope \\\"local\\\"\\n\"");
+
+/* Exported library functions and function blocks */
+
+__asm__(".previous");
+
+
+#endif /* _BUR_1583943259_4_ */
+
